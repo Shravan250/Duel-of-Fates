3604e5d768559e030e94a400eb2fb4ba4e456cde
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-12-30 18:07:13 +0530

    refactor(Home Page): reverted changes for engine testing

--- dueloffates/app/page.tsx
+++ dueloffates/app/page.tsx
@@ -19,38 +19,25 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    const unsubscribe = bindHealthEngine();
-    return unsubscribe;
-    // return () => {
-    //   unsubscribe();
-    // };
-  }, []);
-
-  useEffect(() => {
-    (deckRandomizer());
+    setUserCards(deckRandomizer());
   }, []);
   const {setUserCards}=useCardsContext();
   return (
-    // <div className="min-h-screen bg-gray-100 p-4 md:p-6 lg:p-8">
-    //   <div className="max-w-7xl mx-auto space-y-6">
-    //     <HeadUpDisplay player="Player 2" flip={false} />
+    <div className="min-h-screen bg-gray-100 p-4 md:p-6 lg:p-8">
+      <div className="max-w-7xl mx-auto space-y-6">
+        <HeadUpDisplay player="Player 2" flip={false} />
 
-    //     {/* Opponent Cards - Hidden */}
-    //     <RenderCards user={false} />
+        {/* Opponent Cards - Hidden */}
+        <RenderCards user={false} />
 
-    //     {/* Middle Section - Battle Area */}
-    //     <BattleArea />
+        {/* Middle Section - Battle Area */}
+        <BattleArea />
 
-    //     {/* Player Cards - Visible */}
-    //     <RenderCards user={true} />
+        {/* Player Cards - Visible */}
+        <RenderCards user={true} />
 
-    //     <HeadUpDisplay player="Player 1" flip={true} />
-    //   </div>
-    // </div>
-    <>
-      <HPTest />
-
-      <HPTest />
-    </>
+        <HeadUpDisplay player="Player 1" flip={true} />
+      </div>
+    </div>
   );
 }
