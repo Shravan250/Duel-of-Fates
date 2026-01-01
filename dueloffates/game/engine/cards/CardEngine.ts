3399ab9b5a4fd50834fae9caffec4f518ffcb214
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2026-01-01 23:03:30 +0530

    feat(CardEngine): created a basic card engine template  - added resolve function for cards (Temporary implementation)  - added applyEffects for applying card effects

---
+++ dueloffates/game/engine/cards/CardEngine.ts
@@ -0,0 +1,100 @@
+import { CardDefination, CardInstance } from "@/types";
+import { GameEngine } from "../base/GameEngine";
+import {
+  attackCards,
+  buffDebuffCards,
+  defenceCards,
+  healCards,
+  statusDamage,
+  utilityCards,
+} from "./CardDefinations";
+import { HealthEngine } from "../health/HealthEngine";
+import { ShieldEngine } from "../shield/ShieldEngine";
+
+type cardType =
+  | "attack"
+  | "defense"
+  | "buff"
+  | "debuff"
+  | "status"
+  | "utility"
+  | "heal";
+
+export class cardEngine extends GameEngine {
+  private typeCardMap: Record<cardType, CardDefination[]> = {
+    attack: attackCards,
+    defense: defenceCards,
+    buff: buffDebuffCards,
+    debuff: buffDebuffCards,
+    utility: utilityCards,
+    status: statusDamage,
+    heal: healCards,
+  };
+  private healthEngine: HealthEngine;
+  private shieldEngine: ShieldEngine;
+  constructor(healthEngine: HealthEngine, shieldEngine: ShieldEngine) {
+    super();
+    this.healthEngine = healthEngine;
+    this.shieldEngine = shieldEngine;
+  }
+  public resolve(
+    playerCardInstance: CardInstance,
+    opponentCardInstance: CardInstance
+  ) {
+    const playerCardType = this.getCardType(playerCardInstance.definitionId);
+    const opponentCardType = this.getCardType(
+      opponentCardInstance.definitionId
+    );
+
+    const playerCard = this.getCard(
+      playerCardInstance.definitionId,
+      playerCardType
+    );
+    const opponentCard = this.getCard(
+      opponentCardInstance.definitionId,
+      opponentCardType
+    );
+
+    if (!playerCard || !opponentCard) {
+      return;
+    }
+
+    if (playerCard.priority >= opponentCard.priority) {
+      this.applyEffects("player", playerCard);
+      this.applyEffects("opponent", opponentCard);
+    } else {
+      this.applyEffects("opponent", opponentCard);
+      this.applyEffects("player", playerCard);
+    }
+  }
+
+  private applyEffects(role: "player" | "opponent", card: CardDefination) {
+    let target: "player" | "opponent" = role;
+    if (card.type === "attack") {
+      target = this.invertRole(role);
+      
+      const remDamage = this.shieldEngine.absorbShield(card.damage!, target);
+      remDamage > 0 && this.healthEngine.damage(remDamage, target);
+    }
+    else if(card.type ==='defense'){
+        this.shieldEngine.gainShield(card.shield_gain!, role);
+    }
+    else if(card.type==='heal'){
+        this.healthEngine.heal(card.damage!, role);
+    }
+  }
+
+  private getCardType(cardId: string): cardType {
+    return cardId.split("_")[0] as cardType;
+  }
+
+  private getCard(cardId: string, cardType: cardType) {
+    return this.typeCardMap[cardType].find(
+      (card) => card.definitionId === cardId
+    );
+  }
+
+  private invertRole(role: "player" | "opponent") {
+    return role === "player" ? "opponent" : "player";
+  }
+}
