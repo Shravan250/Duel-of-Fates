9f0685b804b3797965b3c011c777278a61fa35b0
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2026-01-01 20:12:36 +0530

    feat(health engine): modified health engine  - added opponentHp to engine and store  - modified the binder to the new controller based format  - Updated getHp and added resetHp

--- dueloffates/game/engine/health/HealthEngine.ts
+++ dueloffates/game/engine/health/HealthEngine.ts
@@ -1,27 +1,37 @@
 import { GameEngine } from "../base/GameEngine";
 
 export class HealthEngine extends GameEngine {
-  private hp: number;
+  private playerHp: number;
+  private opponentHp: number;
 
-  // initialHp assign
+  // initialise Health engine
   constructor(hp: number) {
     super();
-    this.hp = hp;
+    this.playerHp = hp;
+    this.opponentHp = hp;
   }
 
-  // damage
-  damage(amount: number) {
-    this.hp = Math.max(0, this.hp - amount);
+  // ? reduce health based on damage taken
+  damage(amount: number, target: "player" | "opponent") {
+    if (target === "player") this.playerHp = this.playerHp - amount;
+    else this.opponentHp = this.opponentHp - amount;
     this.notify();
   }
 
-  // heal
-  heal(amount: number) {
-    this.hp = this.hp + amount;
+  // ? regen health based on amount
+  heal(amount: number, target: "player" | "opponent") {
+    if (target === "player") this.playerHp = this.playerHp + amount;
+    else this.opponentHp = this.opponentHp + amount;
+    this.notify();
+  }
+
+  reset(hp: number) {
+    this.playerHp = hp;
+    this.opponentHp = hp;
     this.notify();
   }
 
   getHp() {
-    return this.hp;
+    return { player: this.playerHp, opponent: this.opponentHp };
   }
 }
