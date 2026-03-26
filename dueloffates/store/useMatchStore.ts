823dc4d7e8adc666f202417b24518e54b7b8537c
Shravan250 <shravanbobade25@gmail.com>
2026-03-26 23:01:03 +0530

    updated tsconfig file

--- dueloffates/store/useMatchStore.ts
+++ dueloffates/store/useMatchStore.ts
@@ -1,64 +1,3 @@
-// import { matchEngineController } from "@/game/binders/bindMatchEngine";
-// import { CardProps } from "@/types";
-// import { create } from "zustand";
-
-// type MatchPhase = "SETUP" | "PLAY" | "RESOLVE" | "END";
-
-// interface MatchStoreState {
-//   phase: MatchPhase;
-//   currentTurn: number;
-//   isMatchOver: boolean;
-//   winner: "PLAYER" | "OPPONENT" | null;
-//   timer: number;
-//   isPaused:boolean;
-
-//   // // selection
-//   // selectedPlayerCard: CardProps | null;
-//   // selectedOpponentCard: CardProps | null;
-
-//   // permissoin
-//   canSelectCard: boolean;
-
-//   // controller
-//   matchController: matchEngineController | null;
-
-//   //actions
-//   bindMatchController: (controller: matchEngineController | null) => void;
-//   // selectCard: (card: CardProps, side: "PLAYER" | "OPPONENT") => void;
-//   startMatch: () => void;
-// }
-
-// export const useMatchStore = create<MatchStoreState>((set, get) => ({
-//   // initial state
-//   phase: "SETUP",
-//   currentTurn: 0,
-//   isMatchOver: false,
-//   winner: null,
-//   selectedPlayerCard: null,
-//   selectedOpponentCard: null,
-//   canSelectCard: false,
-//   matchController: null,
-//   timer: 15,
-//   isPaused:false,
-
-//   // bind Controller
-//   bindMatchController: (controller) => {
-//     set({ matchController: controller });
-//   },
-
-//   // public Actions
-//   startMatch: () => {
-//     const { matchController } = get();
-
-//     if (!matchController) {
-//       console.log("matchController not Bound");
-//       return;
-//     }
-
-//     matchController.startMatch();
-//   },
-// }));
-
 import { create } from "zustand";
 
 type MatchPhase = "SETUP" | "PLAY" | "RESOLVE" | "END";
@@ -67,7 +6,7 @@ interface MatchStoreState {
   phase: MatchPhase;
   currentTurn: number;
   isMatchOver: boolean;
-  winner: "PLAYER" | "OPPONENT" | null;
+  winner: "PLAYER" | "OPPONENT" | "DRAW" | null;
   timer: number;
   isPaused: boolean;
   canSelectCard: boolean;
@@ -82,7 +21,7 @@ interface MatchStoreState {
     canSelectCard: boolean;
   }) => void;
 
-  setWinner: (winner: "PLAYER" | "OPPONENT" | null) => void;
+  setWinner: (winner: "PLAYER" | "OPPONENT" | "DRAW" | null) => void;
 
   reset: () => void;
 }
